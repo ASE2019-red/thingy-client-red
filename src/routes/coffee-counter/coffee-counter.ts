c156a88f1f75dc2fc8926a384016ad472ef1b791
pf15ese <pf15ese@users.noreply.github.com>
2019-12-07 15:50:20 +0100

    Can now select machine and time range

--- src/routes/coffee-counter/coffee-counter.ts
+++ src/routes/coffee-counter/coffee-counter.ts
@@ -4,45 +4,113 @@ import { MachineService } from 'resources/machine-service';
 import { MeasurementService } from 'resources/measurement-service';
 import * as Chart from 'chart.js';
 import * as moment from 'moment';
-import { labeledStatement } from '@babel/types';
 
+type ChartTimeMode = 'day' | 'week' | 'month' | 'year';
 @autoinject
 export class CoffeeCounter {
-    private heading = 'Coffee Counter';
-    private allCoffees: string;
-    private allCoffeesOfSpecificMachine: string;
+    private selectedChartMachine: null | string = null;
     private machines;
-    private points = [];
     private chartEl: HTMLCanvasElement;
     private chart: Chart;
     private data: Chart.ChartData;
+    private producedInSelection = 0;
+    private selectedChartTimeMode: ChartTimeMode = 'week';
+    private chartTimeModes = [
+        { value: 'day', text: 'last 24 hours' },
+        { value: 'week', text: 'last 7 days' },
+        { value: 'month', text: 'last month' },
+        { value: 'year', text: 'last year' },
+    ];
+
+    public get consumptionMessage() {
+        return `Coffees produced in the ${this.currentModeText} by ${this.currentMachineText}: `;
+    }
+
+    public get currentModeText() {
+        return this.chartTimeModes.find(m => m.value === this.selectedChartTimeMode).text;
+    }
+
+    public get currentMachineText() {
+        return this.selectedChartMachine == null ? 'all machines' :
+            this.machines.find(m => m.id === this.selectedChartMachine).name;
+    }
 
     constructor(private coffeeService: CoffeeService,
         private machineService: MachineService,
         private measurementService: MeasurementService) {
-        this.coffeeService.getAll().then(response => response.json())
+        this.refreshChart();
+        this.getAllMachines();
+    }
+
+    public onSelectedChartTimeModeChange(mode: ChartTimeMode) {
+        this.selectedChartTimeMode = mode;
+        this.refreshChart();
+    }
+
+    public onSelectedChartMachineChanged(machineId: string | null) {
+        this.selectedChartMachine = machineId;
+        this.refreshChart();
+    }
+
+    public refreshChart() {
+        const currentChartTimeMode = this.selectedChartTimeMode;
+        this.getCoffeesPromise()
+            .then(response => response.json())
             .then(coffees => {
-                this.data = this.generateData(coffees);
+                this.data = this.generateChartData(coffees, currentChartTimeMode);
+                if (this.chart) {
+                    this.chart.data = this.data;
+                    this.chart.update();
+                }
             });
     }
 
-    public generateData(coffees) {
+    public generateChartData(coffees, mode: ChartTimeMode = 'week'): Chart.ChartData {
         const today = moment();
         const dateLabels = [];
-        const coffeeCount = Array(7).fill(0);
-        const aWeekAgo = moment(today).subtract(6, "days");
-        for (const m = moment(aWeekAgo); m.isSameOrBefore(today); m.add(1, 'days')) {
-            dateLabels.push(m.format('DD.M'));
+        let timeUnit: moment.unitOfTime.DurationConstructor = 'days';
+        let timeDifference = 6;
+        let timeFormat = 'DD.M';
+        switch (mode) {
+            case 'day':
+                timeUnit = 'hours';
+                timeDifference = 23;
+                timeFormat = 'HH';
+                break;
+            case 'week':
+                timeUnit = 'days';
+                timeDifference = 6;
+                timeFormat = 'ddd';
+                break;
+            case 'month':
+                timeUnit = 'days';
+                timeDifference = 50;
+                timeFormat = 'DD.M';
+                break;
+            case 'year':
+                timeUnit = 'month';
+                timeDifference = 11;
+                timeFormat = 'MMM';
+                break;
+        }
+        const coffeeCount = Array(timeDifference + 1).fill(0);
+
+        const aWeekAgo = moment(today).subtract(timeDifference, timeUnit);
+        for (const m = moment(aWeekAgo); m.isSameOrBefore(today); m.add(1, timeUnit)) {
+            dateLabels.push(m.format(timeFormat));
         }
         coffees.forEach(c => {
-            const daysDifference = moment(c.createdAt).diff(today, 'days');
-            if (daysDifference >= -6) {
-                coffeeCount[daysDifference + 6] = coffeeCount[daysDifference + 6] + 1;
+            const daysDifference = moment(c.createdAt).diff(today, timeUnit);
+            if (daysDifference >= -timeDifference) {
+                coffeeCount[daysDifference + timeDifference] = coffeeCount[daysDifference + timeDifference] + 1;
             }
         });
+        this.producedInSelection = coffeeCount.reduce((sum, curr) => (sum + curr), 0);
         return {
             labels: dateLabels,
             datasets: [{
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
                 label: 'Coffees consumed',
                 data: coffeeCount,
             }],
@@ -61,7 +129,24 @@ export class CoffeeCounter {
                 scales: {
                     xAxes: [{
                         display: true,
+                        ticks: {
+                            min: 0,
+                            stepSize: 1,
+                        },
                     }],
+                    yAxes: [
+                        {
+                            // typing seems to be wrong here,
+                            // thus we need to cast it to any to ignore it
+                            // it seems to work as appropriate,
+                            // we only get integer ticks with it
+                            ticks: {
+                                beginAtZero: true,
+                                precision: 0,
+                                min: 0,
+                            } as any,
+                        },
+                    ],
                 },
                 plugins: {
                     colorschemes: {
@@ -73,24 +158,11 @@ export class CoffeeCounter {
         });
     }
 
-    private getAllCoffees(): void {
-        this.coffeeService.getAll()
-            .then(response => response.json())
-            .then(listOfCoffees => {
-                this.allCoffees = 'All coffees: ' + Object.keys(listOfCoffees).length;
-                console.log('Returned list of coffees:' + listOfCoffees);
-            })
-            .catch(error => {
-                console.log('Error getting list of coffees!');
-            });
-    }
-
-    private getAllCoffeesOfMachine(machineId): void {
-        this.machineService.getCoffeeCount(machineId)
-            .then(response => response.text())
-            .then(text => {
-                this.allCoffeesOfSpecificMachine = 'All coffees of machine ' + machineId + ': ' + parseInt(text);
-            });
+    private getCoffeesPromise() {
+        if (this.selectedChartMachine == null) {
+            return this.coffeeService.getAll();
+        }
+        return this.machineService.getCoffees(this.selectedChartMachine);
     }
 
     private getAllMachines(): void {
